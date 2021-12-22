cb590de10a9db2cd1094f95c70643b3204d3b705
QuixDevf <zunnur.brad@gmail.com>
2021-12-22 18:28:16 +0800

    Code Cleanup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Status;
 use App\Models\Category;
-use App\Models\Item;
+use App\Models\Asset;
 use App\Models\User;
+use App\Models\Event;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,7 +21,16 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         Status::factory(5)->create();
         Category::factory(5)->create();
-        Item::factory(10)->create();
+        Asset::factory(10)->create();
         User::factory(1)->create();
+        Event::factory(10)->create();
+
+        $asset = Asset::all();
+
+        Event::all()->each(function ($event) use ($asset) {
+            $event->asset()->attach(
+                $asset->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
