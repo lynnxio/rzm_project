9ed5aeb83e5a83c62323dcf590a943a97f315794
QuixDevf <zunnur.brad@gmail.com>
2022-01-07 22:13:09 +0800

    Code Cleanup

--- resources/views/pages/asset/create.blade.php
+++ resources/views/pages/asset/create.blade.php
@@ -34,42 +34,16 @@
 			<div class="flex justify-center">
 				<div class="mb-3 w-96">
 					<label for="formFile" class="inline-block mb-2 text-gray-700 form-label">Image:</label>
-					<input type="file" id="formFile" name="image" class="form-control
-                        block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                    ">
+					<input type="file" id="formFile" name="image"
+					       class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
 				</div>
 			</div>
 			<div class="flex justify-center">
 				<div class="mb-3 xl:w-96">
 					<label for="category">Category:</label>
-					<select id="category" class="form-select appearance-none
-      block
-      w-full
-      px-3
-      py-1.5
-      text-base
-      font-normal
-      text-gray-700
-      bg-white bg-clip-padding bg-no-repeat
-      border border-solid border-gray-300
-      rounded
-      transition
-      ease-in-out
-      m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example"
+					<select id="category"
+					        class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+					        aria-label="Default select example"
 					        name="category_id">
 						<option selected disabled>Please choose...</option>
 						@foreach ($categories as $category)
@@ -82,21 +56,9 @@
 			<div class="flex justify-center">
 				<div class="mb-3 xl:w-96">
 					<label for="status">Status:</label>
-					<select id="status" class="form-select appearance-none
-      block
-      w-full
-      px-3
-      py-1.5
-      text-base
-      font-normal
-      text-gray-700
-      bg-white bg-clip-padding bg-no-repeat
-      border border-solid border-gray-300
-      rounded
-      transition
-      ease-in-out
-      m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example"
+					<select id="status"
+					        class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+					        aria-label="Default select example"
 					        name="status_id">
 						<option selected disabled>Please choose...</option>
 						@foreach ($statuses as $status)
@@ -113,16 +75,7 @@
 					>
 					<input
 							type="number"
-							class="form-control block w-full px-3 py-1.5 text-base font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      "
+							class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
 							id="qty_balance"
 							name="qty_balance"
 							placeholder="Number input"
@@ -131,9 +84,8 @@
 			</div>
 			<div class="flex justify-center">
 				<button type="submit"
-				        class=" px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded
-                    shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none
-                    focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Save
+				        class="px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+					Save
 				</button>
 			</div>
 		</form>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', static function () {
     return redirect()->route('assets.index');
 });
@@ -26,6 +25,5 @@ Route::middleware(['auth'])->group(function () {
         Route::resource('events', EventController::class),
     ];
 });
-
 
 require __DIR__ . '/auth.php';
